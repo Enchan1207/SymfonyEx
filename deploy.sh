89a948be13c45f7ea84b71bf1a0d5dd63aece9ac
Enchan <enchant0130@gmail.com>
2021-04-01 07:06:44 +0900

    [Modify] workflow

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,6 @@ yarn encore prod
 # Compress and transfer using SFTP
 echo "Transfer..."
 cd ../
+ssh r-techlab@r-techlab.sakura.ne.jp "pwd"
 tar czf - SymfonyEx | ssh r-techlab@r-techlab.sakura.ne.jp 'tar zxvf - -C /home/r-techlab/symfony'
 
